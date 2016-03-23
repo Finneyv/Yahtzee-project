5b48bf746b7a627ca527d57300aee55901cff8bb
vfinney <vfinney@elevenfiftyconsulting.com>
2016-03-23 09:00:14 -0400

    Added content to project.

---
+++ VicWebsite/VicWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VicWebsite.Startup))]
+namespace VicWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
