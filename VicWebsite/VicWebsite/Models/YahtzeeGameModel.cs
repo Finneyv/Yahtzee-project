95c30d2ef6b967c4cca28c9a24226709641752b5
vfinney <vfinney@elevenfiftyconsulting.com>
2016-03-23 15:55:42 -0400

    Made changes to the Model and controller.

--- VicWebsite/VicWebsite/Models/YahtzeeGameModel.cs
+++ VicWebsite/VicWebsite/Models/YahtzeeGameModel.cs
@@ -7,14 +7,12 @@ namespace VicWebsite.Models
 {
     public class YahtzeeGameModel
     {
-        public string Player1 { get; set; }
-        public string Player2 { get; set; }
+        public string Yahtzee { get; set; }
         public int Dice1 { get; set; }
         public int Dice2 { get; set; }
         public int Dice3 { get; set; }
         public int Dice4 { get; set; }
         public int Dice5 { get; set; }
-        public int Dice6 { get; set; }
         public int Total { get; set; }
     }
 }
