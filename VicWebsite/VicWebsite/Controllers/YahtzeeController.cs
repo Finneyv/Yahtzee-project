3603fdb455b0c7f4dacbf4ca3e2b66a9033531d9
vfinney <vfinney@elevenfiftyconsulting.com>
2016-03-23 09:16:51 -0400

    Added Yahtzee controller, model and View files to project.

---
+++ VicWebsite/VicWebsite/Controllers/YahtzeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VicWebsite.Controllers
+{
+    public class YahtzeeController : Controller
+    {
+        // GET: Yahtzee
+        public ActionResult Game()
+        {
+            return View();
+        }
+    }
+}
