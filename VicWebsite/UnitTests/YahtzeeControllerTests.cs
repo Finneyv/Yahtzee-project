899c9445f67f104d4e15410531f55070515baafb
vfinney <vfinney@elevenfiftyconsulting.com>
2016-03-28 11:37:56 -0400

    Successfully added unit tests to test the function.

--- VicWebsite/UnitTests/YahtzeeControllerTests.cs
+++ VicWebsite/UnitTests/YahtzeeControllerTests.cs
@@ -1,10 +1,38 @@
-﻿namespace UnitTests
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VicWebsite.Controllers;
+using System.Web.Mvc;
+
+namespace VicWebsiteTests
 {
     namespace YahtzeeControllerTests
     {
         public abstract class BaseTests
         {
-            protected VicWebsite.Controllers.YahtzeeController Controller;
+            protected YahtzeeController Controller;
+
+            [TestInitialize]
+            public void Arrange()
+            {
+                Controller = new YahtzeeController() { };
+            }
+        }
+
+        [TestClass]
+        public class GameLogic : BaseTests
+        {
+            private ActionResult Act(int[] Dices)
+            {
+                return Controller.GameLogic(Dices);
+            }
+
+            [TestMethod]
+            public void ShouldReturnYahtzeeIfAllSame()
+            {
+                int[] array = { 6, 6, 6, 6, 6 };
+                var obj = Act(array);
+                var result = obj.ToString();
+                result.Equals("50");
+            }
         }
     }
 }
