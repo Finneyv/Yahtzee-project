bc2a5360f619fe1f4d8c7fb5141b3af94f104f22
vfinney <vfinney@elevenfiftyconsulting.com>
2016-03-31 11:17:15 -0400

    Finally implemented rerolls.  The Yahtzee page is fully functional.

--- VicWebsite/VicWebsite/Models/YahtzeeGameModel.cs
+++ VicWebsite/VicWebsite/Models/YahtzeeGameModel.cs
@@ -8,12 +8,6 @@ namespace VicWebsite.Models
     public class YahtzeeGameModel
     {
         public string Catagory { get; set; }
-        public bool Reroll1 { get; set; }
-        public bool Reroll2 { get; set; }
-        public bool Reroll3 { get; set; }
-        public bool Reroll4 { get; set; }
-        public bool Reroll5 { get; set; }
-        public int TimesReRolled { get; set; }
         public int Dice1 { get; set; }
         public int Dice2 { get; set; }
         public int Dice3 { get; set; }
