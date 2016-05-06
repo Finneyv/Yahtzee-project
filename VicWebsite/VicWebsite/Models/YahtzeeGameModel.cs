718389d9452c775b7b0fb3512145f2c676cc5808
Vic Finney <finneyv09@gmail.com>
2016-05-06 13:05:29 -0400

    Messed around with the Yahtzee game page found out that the javascript is not currently working.  Added some things to the website

--- VicWebsite/VicWebsite/Models/YahtzeeGameModel.cs
+++ VicWebsite/VicWebsite/Models/YahtzeeGameModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,15 @@ namespace VicWebsite.Models
     public class YahtzeeGameModel
     {
         public string Catagory { get; set; }
+        [DisplayName("Dice 1")]
         public int Dice1 { get; set; }
+        [DisplayName("Dice 2")]
         public int Dice2 { get; set; }
+        [DisplayName("Dice 3")]
         public int Dice3 { get; set; }
+        [DisplayName("Dice 4")]
         public int Dice4 { get; set; }
+        [DisplayName("Dice 5")]
         public int Dice5 { get; set; }
         public int Score { get; set; }
     }
